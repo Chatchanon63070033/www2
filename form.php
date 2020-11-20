56f224c8189250d14b1c3d73090d46ce681e0ee4
Chatchanon63070033 <73678877+Chatchanon63070033@users.noreply.github.com>
2020-11-20 11:56:02 +0700

    Update form.php

--- form.php
+++ form.php
@@ -3,23 +3,42 @@
 require_once("connection.php");
 
 ?>
-<!DOCTYPE html>
 <html>
-
 <head>
-    <title>Comment Form</title>
+<title>ITF Lab</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
-
-<body>
-    <form action="insert.php" method="post" id="CommentForm">
-        Name:<br>
-        <input type="text" name="name" id="idName" placeholder="Enter Name"> <br>
-        Comment:<br>
-        <textarea rows="10" cols="20" name="comment" id="idComment" placeholder="Enter Comment"></textarea><br>
-        Link:<br>
-        <input type="text" name="link" id="idLink" placeholder="Enter Link"> <br><br>
-        <input type="submit" id="commentBtn">
-    </form>
+<body class="bg-dark">
+<div class="container">
+  <div class="row">
+    <div class="col m-auto">
+      <div class="col mt-5">
+        <div class="card-fluid rounded bg-light">
+          <div class="card-title">
+            <h4 class="bg-success text-white text-center py-3">Update Form</h>
+          </div>
+          <div class="card-body">
+           
+             <form action="insert.php" method="post">
+                <input type="text" class="form-control mb-2" placeholder=" name " name="name">
+                <input type="text" class="form-control mb-2" placeholder=" Comment " name="comment">
+                <input type="text" class="form-control mb-2" placeholder=" link " name="link">
+                <button class="btn btn-primary" name="update">Submit</button>
+             </form>
+            
+          </div> 
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<?php
+mysqli_close($conn);
+?>
 </body>
-
 </html>
