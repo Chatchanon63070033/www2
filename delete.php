64ef20a0b58811b814e1787ebcbb1e99638dad5a
Chatchanon63070033 <73678877+Chatchanon63070033@users.noreply.github.com>
2020-11-20 12:15:34 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -9,7 +9,7 @@ $UserID = $_GET['GetID'];
 $sql = "DELETE FROM guestbook WHERE ID=$UserID";
 
 if ($conn->query($sql) === TRUE) {
-  header("location:index.php");
+  header("location:show.php");
 } else {
   echo "Error deleting record: " . $conn->error;
 }
