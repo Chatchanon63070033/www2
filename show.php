7123f594532b48504b76f079a06027159b6f7784
Chatchanon63070033 <73678877+Chatchanon63070033@users.noreply.github.com>
2020-11-20 11:54:45 +0700

    Update show.php

--- show.php
+++ show.php
@@ -1,36 +1,65 @@
 <html>
 <head>
 <title>ITF Lab</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
-<body>
+<body class="bg-dark">
 <?php
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'markererer.mysql.database.azure.com', 'Chatchanon@markererer', 'Swaggac05', 'ITF-marklab', 3306);
+mysqli_real_connect($conn, 'markererer.mysql.database.azure.com', 'Chatchanon@markererer', 'Swaggac05', 'itf-marklab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
-<table width="600" border="1">
-  <tr>
-    <th width="100"> <div align="center">name</div></th>
-    <th width="350"> <div align="center">Comment </div></th>
-    <th width="150"> <div align="center">link </div></th>
-  </tr>
+<div class="container">
+  <div class="row">
+    <div class="col m-auto>
+      <div class="col mt-5>
+        <table width="600" class="table rounded table-bordered">
+          <thead class="thead-light">
+          <tr class="bg-secondary">
+            <th width="25"> <div align="center">id</div></th>
+            <th width="150"> <div align="center">name</div></th>
+            <th width="300"> <div align="center">Comment </div></th>
+            <th width="175"> <div align="center">link </div></th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
 <?php
-while($Result = mysqli_fetch_array($res))
+          while($Result = mysqli_fetch_array($res))
 {
+           $UserID = $Result['id'];
+           $Name = $Result['name'];
+           $Comment = $Result['Comment'];
+           $Link = $Result['link'];
 ?>
-  <tr>
-    <td><?php echo $Result['name'];?></div></td>
-    <td><?php echo $Result['Comment'];?></td>
-    <td><?php echo $Result['link'];?></td>
-  </tr>
+           <tr class="bg-light">
+             <td><?php echo $Userid;?></div></td>
+             <td><?php echo $name;?></div></td>
+             <td><?php echo $Comment;?></td>
+             <td><?php echo $link;?></td>
+             <td><a href="edit.php?Getid=<?php echo $UserID ?>"><button type="button" class="btn btn-primary">Edit</button></a></td>
+             <td><a href="delete.php?Getid=<?php echo $UserID ?>"><button type="button" class="btn btn-danger">Delete</button></a></td>
+           </tr>
 <?php
 }
 ?>
-</table><a href=“form.php”><button type="button" class="btn btn-success">add</button></a>
+          </thead>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
+<div align="center">
+  <a href="add.php"><button type="button" class="btn btn-success">Add++</button></a>
+</div>
 <?php
 mysqli_close($conn);
 ?>
